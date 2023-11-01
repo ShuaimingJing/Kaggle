7736ac7027b4148b430b0939c38962a79f431fe9
ShuaimingJing <140912320+ShuaimingJing@users.noreply.github.com>
2023-11-01 14:47:57 -0400

    Add files via upload

--- kaggle.py
+++ kaggle.py
@@ -19,6 +19,7 @@ class PredictionsFromData:
             df[col] = df[col].replace(replacement_dict)
 
     def data_processing(self):
+        # rename the column in order for convenience of merging
         self.test.rename(columns={'ID': 'account.id'}, inplace=True)
         acc_dropped = self.account[['account.id', 'no.donations.lifetime']]
         self.train = self.train.merge(acc_dropped, on='account.id', how='left')
@@ -41,20 +42,25 @@ class PredictionsFromData:
         clean_subscriptions = dummy_subscriptions.drop(['multiple.subs', 'package', 'price.level'], axis=1)
         self.replace_boolean_with_binary(clean_subscriptions)
 
+        # merge the result dataframe with train and test sets, then fill the nah values with 0
         self.train = self.train.merge(clean_subscriptions, on='account.id', how='left').fillna(0)
         self.test = self.test.merge(clean_subscriptions, on='account.id', how='left').fillna(0)
 
-    # Define the model and required indices and train it
+    # Define the model, required indices and train it
     def model_running(self):
+        
+        # define the inputs for the model
         x_train = self.train.drop(columns=['account.id', 'label'])
         y_train = self.train['label']
         x_test = self.test.drop(columns=['account.id'])
 
+        # run the model
         model = LogisticRegression()
         model.fit(x_train, y_train)
         y_test = model.predict_proba(x_test)
         result = [item[1] for item in y_test]
 
+        # append the result in the test file and change the names with requirements
         self.test['predicted'] = result
         self.test.rename(columns={'account.id': 'ID'}, inplace=True)
 
@@ -62,7 +68,7 @@ class PredictionsFromData:
     def save_predictions(self, file_path):
         self.test[['ID', 'predicted']].to_csv(file_path, index=False)
 
-# Usage:
+# Usage
 predictor = PredictionsFromData(
     '/Users/shuai/Desktop/AIPI 520/Kaggle Competitiom/for_students/train.csv',
     '/Users/shuai/Desktop/AIPI 520/Kaggle Competitiom/for_students/subscriptions.csv',
@@ -71,4 +77,6 @@ predictor = PredictionsFromData(
 )
 predictor.data_processing()
 predictor.model_running()
+
+# Save the result as csv file
 predictor.save_predictions('test_with_predictions.csv')
